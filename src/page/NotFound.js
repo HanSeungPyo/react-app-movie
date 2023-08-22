fe54fc520eed900aa49cb2b611d473aa2d0e09d9
SeungPyo Han <dhffl1234@naver.com>
2023-08-22 20:28:30 +0900

    notfound 컴포넌트 리팩토링

--- src/page/NotFound.js
+++ src/page/NotFound.js
@@ -1,36 +1,35 @@
-import React from 'react'
-import { Container,Row,Col, Button } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
-//404 에러 페이지
 const NotFound = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    const goHome = () =>{
-        navigate("/")
-    }
+  const goHome = () => navigate('/');
 
   return (
-    <>
-        <Container fluid>
-            <Row>
-                <Col>
-                    <div className="error-page">
-                        <div className="error-page-header"><h1>길을 잃으셨나요?</h1></div>    
-                        <div className="error-page-body">
-                            <p>죄송합니다. 해당 페이지를 찾을 수 없습니다.<br/> 홈페이지로 이동해 다양한 콘텐츠를 만나보세요.</p>
-                            <div className="error-page-button">
-                                <Button variant="light" size="xl" onClick={()=>goHome()}> <b>Netflix</b> 홈</Button>
-                            </div>    
-                        </div>    
-                        <div className="error-page-errorCode"><span> 404</span></div>    
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-    </>
-  )
+    <Container fluid>
+      <Row>
+        <Col>
+          <div className="error-page">
+            <h1 className="error-header">길을 잃으셨나요?</h1>
+            <div className="error-body">
+              <p>
+                죄송합니다. 해당 페이지를 찾을 수 없습니다.
+                <br /> 홈페이지로 이동해 다양한 콘텐츠를 만나보세요.
+              </p>
+              <Button variant="light" size="xl" onClick={goHome}> 
+                <b>Netflix</b> 홈
+              </Button>
+            </div>
+            <div className="error-code">
+              <span>404</span>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default NotFound
+export default NotFound;
